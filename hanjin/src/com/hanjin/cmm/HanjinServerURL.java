9c763c1e835b39805f0973863db8de235d69eaef
aberde2806 <aberde2806@gmail.com>
2013-10-17 12:08:51 +0000

    개발 / 운영 경로 추가.

--- hanjin/src/com/hanjin/cmm/HanjinServerURL.java
+++ hanjin/src/com/hanjin/cmm/HanjinServerURL.java
@@ -1,17 +1,44 @@
 package com.hanjin.cmm;
 
 public class HanjinServerURL {
+	
+	/** 운영경로 */
+//	/** 비밀번호 암호화 경로 */
+//	final public static String ENCRYPT_PASSWORD_URL = "http://203.246.159.172:8292/SSO/Login.do";
+//	
+//	/** SSO 로그인 경로 */
+//	final public static String SSO_LOGIN_URL = "http://nmo.hanjin.com:8000/siteminderagent/forms/login.fcc";
+//	
+//	/** GW mail 로그인 경로 */
+//	final public static String GW_MAIL_LOGIN_URL = "http://203.246.159.172:8292/ups/mailservice.do";
+//
+//	/** GW mail mailcount 경로 */
+//	final public static String GW_MAIL_MAILCOUNT_URL = "http://203.246.159.172:8292/ups/mailservice.do";
+//	
+//	/** E-mail 로그인 경로 */
+//	final public static String EMAIL_LOGIN_URL = "http://mail.hanjin.com/mobile/ext/login.xml";
+//	
+//	/** E-mail mailcount 경로 */
+//	final public static String EMAIL_MAILCOUNT_URL = "http://mail.hanjin.com/mobile/ext/list.xml";
+//	
+//	/** Approval count 경로 */
+//	final public static String APPROVAL_COUNT_URL = "http://gw.hanjin.com/ezGateWayM/ezApproval.asmx?WSDL";
+//	
+//	/** BulletinBoard count 경로 */
+//	final public static String BULLETINBOARD_COUNT_URL = "http://gw.hanjin.com/ezGateWayM/ezBoardSTD.asmx?WSDL";
+	
+	/** 개발 경로 */
 	/** 비밀번호 암호화 경로 */
-	final public static String ENCRYPT_PASSWORD_URL = "http://nmo.hanjin.com:8292/SSO/Login.do";
+	final public static String ENCRYPT_PASSWORD_URL = "http://203.245.154.153:8292/SSO/Login.do";
 	
 	/** SSO 로그인 경로 */
 	final public static String SSO_LOGIN_URL = "http://nmo.hanjin.com:8000/siteminderagent/forms/login.fcc";
 	
 	/** GW mail 로그인 경로 */
-	final public static String GW_MAIL_LOGIN_URL = "http://nmo.hanjin.com:8292/ups/mailservice.do";
+	final public static String GW_MAIL_LOGIN_URL = "http://203.245.154.153:8292/ups/mailservice.do";
 
 	/** GW mail mailcount 경로 */
-	final public static String GW_MAIL_MAILCOUNT_URL = "http://nmo.hanjin.com:8292/ups/mailservice.do";
+	final public static String GW_MAIL_MAILCOUNT_URL = "http://203.245.154.153:8292/ups/mailservice.do";
 	
 	/** E-mail 로그인 경로 */
 	final public static String EMAIL_LOGIN_URL = "http://mail.hanjin.com/mobile/ext/login.xml";
@@ -24,5 +51,4 @@ public class HanjinServerURL {
 	
 	/** BulletinBoard count 경로 */
 	final public static String BULLETINBOARD_COUNT_URL = "http://gw.hanjin.com/ezGateWayM/ezBoardSTD.asmx?WSDL";
-	
 }
