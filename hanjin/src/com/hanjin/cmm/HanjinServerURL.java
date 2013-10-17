4fff47be8857c54d545f296aeb75e8a307583405
aberde2806 <aberde2806@gmail.com>
2013-10-17 12:18:17 +0000

    IP 수정

--- hanjin/src/com/hanjin/cmm/HanjinServerURL.java
+++ hanjin/src/com/hanjin/cmm/HanjinServerURL.java
@@ -29,16 +29,16 @@ public class HanjinServerURL {
 	
 	/** 개발 경로 */
 	/** 비밀번호 암호화 경로 */
-	final public static String ENCRYPT_PASSWORD_URL = "http://203.245.154.153:8292/SSO/Login.do";
+	final public static String ENCRYPT_PASSWORD_URL = "http://203.246.154.153:8292/SSO/Login.do";
 	
 	/** SSO 로그인 경로 */
 	final public static String SSO_LOGIN_URL = "http://nmo.hanjin.com:8000/siteminderagent/forms/login.fcc";
 	
 	/** GW mail 로그인 경로 */
-	final public static String GW_MAIL_LOGIN_URL = "http://203.245.154.153:8292/ups/mailservice.do";
+	final public static String GW_MAIL_LOGIN_URL = "http://203.246.154.153:8292/ups/mailservice.do";
 
 	/** GW mail mailcount 경로 */
-	final public static String GW_MAIL_MAILCOUNT_URL = "http://203.245.154.153:8292/ups/mailservice.do";
+	final public static String GW_MAIL_MAILCOUNT_URL = "http://203.246.154.153:8292/ups/mailservice.do";
 	
 	/** E-mail 로그인 경로 */
 	final public static String EMAIL_LOGIN_URL = "http://mail.hanjin.com/mobile/ext/login.xml";
