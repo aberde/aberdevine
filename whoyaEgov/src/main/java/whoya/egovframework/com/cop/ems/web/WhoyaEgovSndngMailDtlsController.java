5ae2e152479d0093a27272ce1d2db7e61227d989
aberde2806 <aberde2806@gmail.com>
2014-02-14 08:46:15 +0000

    발송메일을 삭제한다.

--- whoyaEgov/src/main/java/whoya/egovframework/com/cop/ems/web/WhoyaEgovSndngMailDtlsController.java
+++ whoyaEgov/src/main/java/whoya/egovframework/com/cop/ems/web/WhoyaEgovSndngMailDtlsController.java
@@ -3,6 +3,8 @@ package whoya.egovframework.com.cop.ems.web;
 import java.util.List;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import net.sf.json.JSONObject;
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import whoya.whoyaDataProcess;
 import whoya.whoyaLib;
 import whoya.whoyaList;
 import whoya.whoyaMap;
@@ -20,6 +23,7 @@ import whoya.common.Common;
 import whoya.common.commonReturn;
 import whoya.egovframework.com.cop.ems.service.WhoyaEgovSndngMailDtlsService;
 import egovframework.com.cmm.ComDefaultVO;
+import egovframework.com.cop.ems.service.SndngMailVO;
 
 /**
  * 발송메일 내역을 조회하는 컨트롤러 클래스
@@ -58,8 +62,8 @@ public class WhoyaEgovSndngMailDtlsController {
 			List voList = sndngMailDtlsService.selectSndngMailList(searchVO);
 			whoyaList list = new whoyaList(Common.ConverObjectToWhoyaMap(voList));
 			
-			// 상태,발신자,수신자,제목,날짜
-		 	resultList.put("list", whoyaLib.whoyaRenderGrid(list, "sndngResultCode,dsptchPerson,recptnPerson,sj,sndngDe"));
+			// 상태,발신자,수신자,제목,날짜,메세지ID
+		 	resultList.put("list", whoyaLib.whoyaRenderGrid(list, "sndngResultCode,dsptchPerson,recptnPerson,sj,sndngDe,mssageId"));
 		 	resultList.put("status", commonReturn.SUCCESS);
 		 	resultList.put("message", "조회되었습니다");
 		} catch(Exception e) {
@@ -70,32 +74,45 @@ public class WhoyaEgovSndngMailDtlsController {
 		
 		return resultList;
 	}
-//	
-//	/** EgovPropertyService */
-//    @Resource(name = "propertiesService")
-//    protected EgovPropertyService propertiesService;
-//    
-//    /** EgovMessageSource */
-//    @Resource(name="egovMessageSource")
-//    EgovMessageSource egovMessageSource;
-//    
-//    /**
-//	 * 발송메일을 삭제한다.
-//	 * @param sndngMailVO SndngMailVO
-//	 * @return String
-//	 * @exception
-//	 */
-//    @RequestMapping(value="/cop/ems/deleteSndngMailList.do")
-//	public String deleteSndngMailList(@ModelAttribute("sndngMailVO") SndngMailVO sndngMailVO, ModelMap model) throws Exception {
-//    	
-//    	if (sndngMailVO == null || sndngMailVO.getMssageId() == null || sndngMailVO.getMssageId().equals("")) {
-//    		return "egovframework/com/cmm/egovError";
-//    	}
-//    	
-//    	// 1. 발송메일을 삭제한다.
-//    	sndngMailDtlsService.deleteSndngMailList(sndngMailVO);
-//    	
-//        // 2. 발송메일 목록 페이지 이동
-//    	return "redirect:/cop/ems/selectSndngMailList.do";
-//	}
+	
+	/**
+	 * 발송메일을 삭제한다.
+	 * @param sndngMailVO SndngMailVO
+	 * @return String
+	 * @exception
+	 */
+	@RequestMapping(value="/whoya/cop/ems/deleteSndngMailList.do")
+	public @ResponseBody void deleteSndngMailList(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		try {
+    		String[] ids = request.getParameter("ids").split(",");
+    		
+    		whoyaDataProcess  data = new whoyaDataProcess();
+    	    whoyaMap rows = new whoyaMap();
+    	    rows = data.dataProcess(request, response);
+    	    
+    	    String mssageId = "";
+    	    String atchFileIdList = "";
+    		for (int i = 0; i < ids.length; i++) {
+    			whoyaMap cols = (whoyaMap) rows.get(ids[i]);
+    			SndngMailVO sndngMailVO = new SndngMailVO();
+    			sndngMailVO = (SndngMailVO)Common.convertWhoyaMapToObject(cols, sndngMailVO);
+
+    			if (sndngMailVO == null || sndngMailVO.getMssageId() == null || sndngMailVO.getMssageId().equals("")) {
+    	    		throw new Exception("선택된 데이터가 없습니다.");
+    	    	}
+    			
+    			mssageId += sndngMailVO.getMssageId() + ",";
+    			atchFileIdList += sndngMailVO.getAtchFileIdList() + ",";
+    		}
+    		
+    		SndngMailVO sndngMailVO = new SndngMailVO();
+    		sndngMailVO.setMssageId(mssageId.substring(0, mssageId.length() - 1));
+    		sndngMailVO.setAtchFileIdList(atchFileIdList.substring(0, atchFileIdList.length() - 1));
+    		// 1. 발송메일을 삭제한다.
+    		sndngMailDtlsService.deleteSndngMailList(sndngMailVO);
+    	} catch ( Exception e ) {
+    		e.printStackTrace();
+    		throw e;
+    	}
+	}
 }
